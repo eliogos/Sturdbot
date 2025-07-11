8f3f3d2e49542ff4d2bd8a65b4a0722e2c396de7
Eli <37794052+eliogos@users.noreply.github.com>
2025-07-12 01:00:53 +0800

    Update ping.js

--- commands/slash/user/general/ping.js
+++ commands/slash/user/general/ping.js
@@ -1,9 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong and response time!'),
+
+	targets: ['bot_1', 'bot_2'], // Allow both bots to use this command
+
+	async execute(interaction, client) {
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		const diff = sent.createdTimestamp - interaction.createdTimestamp;
+		await interaction.editReply(`Pong from **${client.label}**! 🏓 ${diff}ms`);
+	}
 };
